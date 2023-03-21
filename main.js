1f9bee7fffcb87ca89ff5ac01dba508948946700
JW-400 <julian.wasmeier@outlook.com>
2023-03-21 03:45:37 +0100

    feat: add canvas boilerplate, remove unneccessary files

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/** @type {HTMLCanvasElement} */
+import {
+  randomIntFromRange,
+  randomItemFromArray,
+  randomRgbColor,
+} from './helper';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-setupCounter(document.querySelector('#counter'))
+let width = (canvas.width = window.innerWidth);
+let height = (canvas.height = window.innerHeight);
+
+let mouse = { x: undefined, y: undefined };
+
+const colors = ['#DB488B', '#1F1A70', '#1A0E3E', '#3337C0', '#D854C2'];
+
+window.addEventListener('resize', (e) => {
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+});
+
+window.addEventListener('mousemove', (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+class Object {
+  constructor(x, y, dx, dy, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function animate() {
+  ctx.fillStyle = 'rgba(255,255,255)';
+  ctx.fillRect(0, 0, width, height);
+  new Object(10, 10, 10, 10, 10, colors[0]).draw(ctx);
+  requestAnimationFrame(animate);
+}
+animate();
